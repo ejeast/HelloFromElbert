d2f5befe62319b7f5417e11fc25c16671d2fee09
acristeche <cristechearturo@gmail.com>
2022-02-05 16:59:41 -0300

    Greetings from Arturo

--- src/eastmond/Main.java
+++ src/eastmond/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Elbert");
+        System.out.println("Hi Elbert, my name is Arturo");
     }
 }
